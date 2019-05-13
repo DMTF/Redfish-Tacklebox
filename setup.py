ce3dfda319b104cfcfd8afd29f880c8d40f972db
Mike Raineri <michael.raineri@dell.com>
2019-05-13 14:07:40 -0400

    Initial conversion for pypi distribution

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='redfish_utilities',
+      version='0.5.0',
+      description='Redfish Utilities',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author = 'DMTF, https://www.dmtf.org/standards/feedback',
+      license='BSD 3-clause "New" or "Revised License"',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Topic :: Communications'
+      ],
+      keywords='Redfish',
+      url='https://github.com/DMTF/Redfish-Tacklebox',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      scripts=['scripts/sensor_list'],
+      install_requires=[
+          'redfish'
+      ])
---
+++ src/redfish_utilities/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# Copyright Notice:
+# Copyright 2019 DMTF. All rights reserved.
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Tacklebox/blob/master/LICENSE.md
+
+from .sensor_list import get_sensors
+from .sensor_list import print_sensors
