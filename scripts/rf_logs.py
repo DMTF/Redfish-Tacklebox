1fa7f133b41d74c7de1a28166131276329d7409a
Mike Raineri <michael.raineri@dell.com>
2020-04-12 17:09:19 -0400

    Fixed log tool to not hide all exceptions from the console

--- scripts/rf_logs.py
+++ scripts/rf_logs.py
@@ -60,6 +60,8 @@ try:
         # Print log was requested
         log_entries = redfish_utilities.get_log_entries( redfish_obj, container_type, container_id, args.log )
         redfish_utilities.print_log_entries( log_entries, args.details )
+except BrokenPipeError:
+    pass
 finally:
     redfish_obj.logout()
-    sys.stderr.close()
+    #sys.stderr.close()
