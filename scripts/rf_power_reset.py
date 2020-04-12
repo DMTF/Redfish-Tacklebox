d2b909ed895a9faeaf2c3e432d971f2c0e604024
Mike Raineri <michael.raineri@dell.com>
2020-04-12 16:29:44 -0400

    Added option in power/reset script to display reset info

--- scripts/rf_power_reset.py
+++ scripts/rf_power_reset.py
@@ -22,6 +22,7 @@ argget.add_argument( "--password", "-p",  type = str, required = True, help = "T
 argget.add_argument( "--rhost", "-r", type = str, required = True, help = "The address of the Redfish service (with scheme)" )
 argget.add_argument( "--system", "-s", type = str, help = "The ID of the system perform the operation" )
 argget.add_argument( "--type", "-t", type = str, help = "The type of power/reset operation to perform" )
+argget.add_argument( "--info", "-info", action = "store_true", help = "Indicates if reset information should be reported" )
 args = argget.parse_args()
 
 # Set up the Redfish object
@@ -29,10 +30,20 @@ redfish_obj = redfish.redfish_client( base_url = args.rhost, username = args.use
 redfish_obj.login( auth = "session" )
 
 try:
-    print( "Resetting the system..." )
-    response = redfish_utilities.system_reset( redfish_obj, args.system, args.type )
-    response = redfish_utilities.poll_task_monitor( redfish_obj, response )
-    redfish_utilities.verify_response( response )
+    if args.info:
+        reset_uri, reset_parameters = redfish_utilities.get_system_reset_info( redfish_obj, args.system )
+        printed_reset_types = False
+        for param in reset_parameters:
+            if param["Name"] == "ResetType" and "AllowableValues" in param:
+                print( "Supported reset types: {}".format( ", ".join( param["AllowableValues"] ) ) )
+                printed_reset_types = True
+        if not printed_reset_types:
+            print( "No reset information found" )
+    else:
+        print( "Resetting the system..." )
+        response = redfish_utilities.system_reset( redfish_obj, args.system, args.type )
+        response = redfish_utilities.poll_task_monitor( redfish_obj, response )
+        redfish_utilities.verify_response( response )
 finally:
     # Log out
     redfish_obj.logout()
