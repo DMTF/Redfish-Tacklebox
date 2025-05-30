a5e4cd693fc04916e6eb5de4bcb33791c027c374
Mike Raineri <michael.raineri@dell.com>
2025-05-30 10:16:35 -0400

    Added new global config to automatically internalize task monitoring for select operations
    Signed-off-by: Mike Raineri <michael.raineri@dell.com>

--- redfish_utilities/config.py
+++ redfish_utilities/config.py
@@ -9,9 +9,14 @@ Configuration Settings
 File : config.py
 
 Brief : This file contains configuration settings for the module; useful for
-        debugging issues encountered on live systems
+        debugging issues encountered on live systems or configuring global
+        options.
 """
 
 # Leverage known workarounds for non-conformant services, such as bypassing
 # unexpected 4XX responses, missing properties, and malformed URIs
 __workarounds__ = False
+
+# Automate task handling for POST/PATCH/PUT/DELETE operations that should
+# always be "fast"
+__auto_task_handling__ = False
